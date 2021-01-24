752d777e1b4a595c4174205ff7e33ebf56e3a21b
Prokhorov <grisha.prokhorov03@gmail.com>
2021-01-24 18:37:26 +0200

    Factory Method

---
+++ index.ts
@@ -0,0 +1,50 @@
+//Factory Method
+interface newCar {
+      giveNewCar(): string;
+}
+
+abstract class CarShowroom {
+
+      public abstract Method(): newCar;
+
+      public Buy(): string {
+            const product = this.Method();
+
+            return "You buy " + product.giveNewCar() + ", congratulation";
+      }
+}
+
+class Audi extends CarShowroom {
+      public Method(): newCar {
+            return new AudiCar();
+      }
+}
+
+class BMW extends CarShowroom {
+      public Method(): newCar {
+            return new BMWCar();
+      }
+}
+
+class AudiCar implements newCar {
+      public giveNewCar(): string {
+            return "Audi, A7";
+      }
+}
+
+class BMWCar implements newCar {
+      public giveNewCar(): string {
+            return "BMW, M2 competion";
+      }
+}
+
+function code(showroom: CarShowroom) {
+      console.log(showroom.Buy());
+      console.log('');
+}
+
+code(new Audi);
+
+code(new BMW);
+
+
